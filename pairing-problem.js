c58c228f7dfcb644e5e832997fdcb7e92a8d9ec5
bafiam <stevebafiam@gmail.com>
2021-01-05 23:14:10 +0300

    refactor code and optimize it

--- pairing-problem.js
+++ pairing-problem.js
@@ -9,7 +9,6 @@ const gen_collection = (collection, n) => {
     }
     let combinations = [];
     array = [...array];
-
     while (array.length - n) {
       let value = array.shift();
       recur(array, n).forEach((combination) => {
@@ -25,67 +24,77 @@ const gen_collection = (collection, n) => {
 const range = (start, stop) =>
   Array.from({ length: (stop + 1 - start) / 1 }, (_, i) => [start + i]);
 
-// return uniq array given an array
-const get_all_uniq = (uniq, data, P) => {
-  let rest = [];
-  for (let index = 0; index < uniq.length; index++) {
-    const element = uniq[index];
-    len = data.length;
-    elm = data[len - 1];
-    let merged_selected = [].concat.apply([], elm);
-    let merged_transit = [].concat.apply([], element);
-    let group = merged_selected.concat(merged_transit);
-    let unique = [...new Set(group)];
-    if (unique.length === P * 2) {
-      rest.push(element);
-    }
-  }
-  return rest;
-};
-// data processing and time processing 
+// data processing and time processing
 const data_process = (N, P, TpM, T, data) => {
   let results = [];
   rooms = Math.floor(N / P);
   results.push({
     total_number_of_rooms_to_have_meetings_simultaneously: rooms,
   });
-  sessions = Math.floor(data.length);
-  results.push({ total_number_of_possible_sessions: sessions });
-  let time_in_mins = T * 60;
-  results.push({ total_time_of_one_sessions_in_mins: TpM });
-  let total_number_of_session = Math.floor(time_in_mins / TpM);
+  sessions = data.length;
+  results.push({ Unique_groups_of_P: sessions });
+
   results.push({
-    total_number_of_sessions: total_number_of_session,
+    total_number_of_sessions: Math.floor(sessions / rooms),
   });
-  let participants = [];
-  for (let index = 1; index < data.length; index++) {
-    const element = data[index];
-    if (index <= total_number_of_session) {
-      participants.push(element);
+
+  results.push({ participants_in_groups_of_P: JSON.stringify(data) });
+  return results;
+};
+
+const findCommonElement = (array1, array2) => {
+  let count = 0;
+
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      if (array1[i] === array2[j]) {
+        count++;
+      }
     }
   }
 
-  results.push({ participants_per_sessions: participants });
-  return results;
+  return count;
+};
+
+const analyzer = (data_arr) => {
+  let data = [];
+
+  for (let index = 0; index < data_arr.length; index++) {
+    const element = data_arr[index];
+    if (index == 0) {
+      data.push(element);
+    } else {
+      let res = [];
+      let test_selected = [].concat.apply([], element);
+
+      data.forEach((final) => {
+        let final_selected = [].concat.apply([], final);
+
+        let check = findCommonElement(test_selected, final_selected);
+
+        res.push(check);
+      });
+
+      let results = res.filter((num) => num > 1);
+
+      if (results.length === 0) {
+        data.push(element);
+      }
+    }
+  }
+
+  return data;
 };
 // run script
 const run = (N, P, TpM, T) => {
   let people = range(1, N);
-  let uniq = gen_collection(people, P);
-  let data = [];
-  uniq.forEach((elem) => {
-    let test = [];
-    test.push(elem);
-    let uniq_r = get_all_uniq(uniq, test, P);
-    while (uniq_r.length > 0) {
-      test.push(uniq_r[0]);
-      uniq_r = get_all_uniq(uniq_r.slice(1), test, P);
-    }
-    data.push(test);
-  });
 
-  return data_process(N, P, TpM, T, data);
+  let uniq = gen_collection(people, P);
+
+  let final = analyzer(uniq);
+
+  return data_process(N, P, TpM, T, final);
 };
 
-let a = run(6, 3, 20, 2);
+let a = run(20, 4, 20, 2);
 console.log(a);
