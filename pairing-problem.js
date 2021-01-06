063f1d2406533f1a28350ad5b62bea07751b2543
bafiam <stevebafiam@gmail.com>
2021-01-06 21:41:02 +0300

    group the final uniq combo

--- pairing-problem.js
+++ pairing-problem.js
@@ -29,13 +29,12 @@ const data_process = (N, P, TpM, T, data) => {
   let results = [];
   rooms = Math.floor(N / P);
   results.push({
-    total_number_of_rooms_to_have_meetings_simultaneously: rooms,
+    total_number_of_rooms_to_have_meetings_simultaneously: data[0].length,
   });
   sessions = data.length;
-  results.push({ Unique_groups_of_P: sessions });
 
   results.push({
-    total_number_of_sessions: Math.floor(sessions / rooms),
+    total_number_of_possible_sessions: sessions,
   });
 
   results.push({ participants_in_groups_of_P: JSON.stringify(data) });
@@ -85,16 +84,51 @@ const analyzer = (data_arr) => {
 
   return data;
 };
+const get_all_uniq = (uniq, data, P) => {
+  let rest = [];
+  for (let index = 0; index < uniq.length; index++) {
+    const element = uniq[index];
+    len = data.length;
+    elm = data[len - 1];
+    let merged_selected = [].concat.apply([], elm);
+    let merged_transit = [].concat.apply([], element);
+    let group = merged_selected.concat(merged_transit);
+    let unique = [...new Set(group)];
+    if (unique.length === P+P) {
+      rest.push(element);
+    }
+  }
+  return rest;
+};
 // run script
 const run = (N, P, TpM, T) => {
   let people = range(1, N);
 
   let uniq = gen_collection(people, P);
+  let data = []
 
   let final = analyzer(uniq);
+  let copy_final = [...final]
+  for (let index = 0; index < copy_final.length; index++) {
+    let elem = copy_final[index];
+    let final_rest = copy_final.splice(index)
+     let test = [];
+    test.push(elem);
+    let uniq_r = get_all_uniq(final_rest, test, P);
 
-  return data_process(N, P, TpM, T, final);
+    while (uniq_r.length > 0) {
+      test.push(uniq_r[0]);
+      final_stage = uniq_r.slice(1)
+      uniq_r = get_all_uniq(final_stage, test, P);
+    }
+   
+    data.push(test);
+    test = []
+    copy_final = final_rest
+    
+  }
+  return data_process(N, P, TpM, T, data);
 };
 
-let a = run(20, 4, 20, 2);
+let a = run(20, 3, 20, 2);
 console.log(a);
